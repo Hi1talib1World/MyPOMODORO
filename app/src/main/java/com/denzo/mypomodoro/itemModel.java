bdc3f4f2a573cab9003b19c9916e92539075be3c
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-05-06 17:54:47 +0000

    Update itemModel.java

--- app/src/main/java/com/denzo/mypomodoro/itemModel.java
+++ app/src/main/java/com/denzo/mypomodoro/itemModel.java
@@ -1,4 +1,22 @@
 package com.denzo.mypomodoro;
 
 public class itemModel {
+    int image;
+    String name;
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
