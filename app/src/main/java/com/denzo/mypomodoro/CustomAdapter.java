2a06d482240079b2e5f61ec8d5c0f8aa47e7bcff
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-05-06 17:55:22 +0000

    Update CustomAdapter.java

--- app/src/main/java/com/denzo/mypomodoro/CustomAdapter.java
+++ app/src/main/java/com/denzo/mypomodoro/CustomAdapter.java
@@ -1,4 +1,54 @@
 package com.denzo.mypomodoro;
 
-public class CustomAdapter {
+
+import android.content.Context;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.viewHolder> {
+
+    Context context;
+    ArrayList<itemModel> arrayList;
+
+    public CustomAdapter(Context context, ArrayList<itemModel> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+    }
+
+    @Override
+    public  CustomAdapter.viewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.itemlist, viewGroup, false);
+        return new viewHolder(view);
+    }
+    @Override
+    public  void onBindViewHolder(CustomAdapter.viewHolder viewHolder,int position) {
+        viewHolder.iconName.setText(arrayList.get(position).getName());
+        viewHolder.icon.setImageResource(arrayList.get(position).getImage());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder {
+        ImageView icon;
+        TextView iconName;
+
+        public viewHolder(View itemView) {
+            super(itemView);
+            icon = (ImageView) itemView.findViewById(R.id.icon);
+            iconName = (TextView) itemView.findViewById(R.id.icon_name);
+
+        }
+    }
+
 }
