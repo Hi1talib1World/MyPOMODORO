e537739bd4e14be3496adb90ef25c3da7e0e93a8
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-11 22:45:36 +0100

    Create CustomBottomSheetDialogFragment.java

---
+++ app/src/main/java/com/denzo/mypomodoro/CustomBottomSheetDialogFragment.java
@@ -0,0 +1,26 @@
+package com.denzo.mypomodoro;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+
+public class CustomBottomSheetDialogFragment extends BottomSheetDialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.bottom_sheet, container,false);
+        return v;
+    }
+}
