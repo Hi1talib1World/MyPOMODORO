aea6cd1abe365df97bd78326a74f4cee5bdff082
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-13 18:49:16 +0100

    Create addpomodoro.java

---
+++ app/src/main/java/com/denzo/mypomodoro/Tasks/addpomodoro.java
@@ -0,0 +1,16 @@
+package com.denzo.mypomodoro.Tasks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.denzo.mypomodoro.R;
+
+public class addpomodoro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addpomodoro);
+    }
+}
