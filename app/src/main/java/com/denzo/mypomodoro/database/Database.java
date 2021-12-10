b9a1fea2a18537746f9aef1b8f1855482927e48e
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2021-12-11 00:30:43 +0100

    Create Database.java

---
+++ app/src/main/java/com/denzo/mypomodoro/database/Database.java
@@ -0,0 +1,40 @@
+package com.denzo.mypomodoro.database;
+
+import android.app.Activity;
+import android.content.Context;
+
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@androidx.room.Database(entities = {Pomodoro.class, Activity.class}, version = 1)
+public abstract class Database extends RoomDatabase {
+
+    private static volatile Database database;
+    public static final ExecutorService databaseExecutor =
+            Executors.newSingleThreadExecutor();
+
+    private static Database buildDatabaseInstance(Context context) {
+        if (database != null) {
+            throw new RuntimeException("Use getInstance() method to get the single instance of " +
+                    "this class.");
+        }
+        return Room.databaseBuilder(context, Database.class, Constants.DATABASE_NAME).build();
+    }
+
+    public static Database getInstance(Context context) {
+        if (database == null) {
+            synchronized (Database.class) {
+                if (database == null) {
+                    database = buildDatabaseInstance(context);
+                }
+            }
+        }
+        return database;
+    }
+
+    public abstract PomodoroDao pomodoroDao();
+
+    public abstract ActivityDao activityDao();
+}
