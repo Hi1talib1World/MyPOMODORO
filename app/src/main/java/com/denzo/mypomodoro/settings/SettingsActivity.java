b61032b9b15e100c2b5cecd96ee87d9c1985e433
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-06-07 02:22:38 +0100

    feat: update SettingsActivity logic

--- app/src/main/java/com/denzo/mypomodoro/settings/SettingsActivity.java
+++ app/src/main/java/com/denzo/mypomodoro/settings/SettingsActivity.java
@@ -9,14 +9,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.denzo.mypomodoro.MainActivity;
 
 
+import com.denzo.mypomodoro.MainActivity;
+import com.denzo.mypomodoro.R;
+import com.denzo.mypomodoro.activities.Activities;
+import com.denzo.mypomodoro.statistics.StatisticsActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.content.Intent;
+
+
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_nav);
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setSelectedItemId(R.id.nav_settings);
+        bottomNavigation.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_focus) {
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            } else if (itemId == R.id.nav_tasks) {
+                startActivity(new Intent(this, Activities.class));
+                return true;
+            } else if (itemId == R.id.nav_stats) {
+                startActivity(new Intent(this, StatisticsActivity.class));
+                return true;
+            } else if (itemId == R.id.nav_settings) {
+                return true;
+            }
+            return false;
+        });
+
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+                .replace(R.id.settings_container, new SettingsFragment())
                 .commit();
     }
 
