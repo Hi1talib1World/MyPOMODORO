10839b7c474963c43f597ff16bb1147aff29c15e
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-06-10 06:26:31 +0100

    feat: update PomodoroReceiver logic

--- app/src/main/java/com/denzo/mypomodoro/PomodoroReceiver.java
+++ app/src/main/java/com/denzo/mypomodoro/PomodoroReceiver.java
@@ -3,22 +3,31 @@ package com.denzo.mypomodoro;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
 
 public class PomodoroReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        int activityId = prefs.getInt(Constants.CURRENT_ACTIVITY_ID, 1);
+
+        Intent actionIntent = new Intent(context, TimerActionReceiver.class);
+        actionIntent.putExtra(Constants.CURRENT_ACTIVITY_ID_INTENT, activityId);
 
         if ("com.denzo.mypomodoro.PLAY".equals(action)) {
-            // Handle the play action
-            // Start the timer or resume it
-            Intent serviceIntent = new Intent(context, PomodoroService.class);
-            context.startService(serviceIntent);
+            boolean isRunning = prefs.getBoolean(Constants.IS_TIMER_RUNNING, false);
+            if (isRunning) {
+                actionIntent.putExtra(Constants.BUTTON_ACTION, Constants.BUTTON_PAUSE);
+            } else {
+                actionIntent.putExtra(Constants.BUTTON_ACTION, Constants.BUTTON_START);
+            }
+            context.sendBroadcast(actionIntent);
         } else if ("com.denzo.mypomodoro.STOP".equals(action)) {
-            // Handle the stop action
-            // Stop the timer or pause it
-            context.stopService(new Intent(context, PomodoroService.class));
+            actionIntent.putExtra(Constants.BUTTON_ACTION, Constants.BUTTON_STOP);
+            context.sendBroadcast(actionIntent);
         }
     }
 }
